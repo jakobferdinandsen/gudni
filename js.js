19f0eb19d7a7690001536ddb446e8e192b9aca27
Jakob Ferdinandsen <jakob@jhaaf.dk>
2018-05-27 11:19:30 +0200

    Updated shakiness

--- js.js
+++ js.js
@@ -4,8 +4,8 @@ $(function () {
         $('.gudni').addClass('shake-slow').addClass('shake-constant');
         setTimeout(function () {
             $('.gudni').removeClass('shake-slow').addClass('shake-crazy');
-        }, 120000)
-    }, 120000)
+        }, 60000)
+    }, 60000)
 
     $(document).on('click', '.gudni', function () {
         if ($(this).attr('src') == "IMG_0449.JPG") {
